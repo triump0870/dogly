424864e3d68a695ce53b8ba658cfb49b0781eb97
Rohan Roy <b3you0870@gmail.com>
2017-09-17 03:53:26 +0530

    visit model was added

--- src/app/models.py
+++ src/app/models.py
@@ -30,4 +30,59 @@ class Dog(models.Model):
         except IntegrityError as e:
             print "error"
             raise IntegrityError("Dog with name [%s %s] already exists" % (self.first_name, self.last_name))
-            
+
+
+class Visit(models.Model):
+    dog = models.ForeignKey(Dog)
+    start_date = models.DateField(null=False, blank=False)
+    end_date = models.DateField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "#%s-%s" % (str(self.id), str(self.dog))
+
+    def save(self, *args, **kwargs):
+        title = "new dog visit"
+        description = "dog [%s] has a visit" % self.dog.id
+        new_visit = False
+        if not self.id:
+            new_visit = True
+
+        visits = Visit.objects.filter(dog=self.dog).filter(
+            Q(start_date__range=(self.start_date, self.end_date))
+        )
+        if visits.exists():
+            raise ValidationError('Overlapping boarding visit for dog [%s]' % self.dog.id)
+
+        super(Visit, self).save(*args, **kwargs)
+
+        if new_visit:
+            event = Event(
+                start=self.start_date,
+                end=self.end_date,
+                title=title,
+                description=description
+            )
+            event.save()
+            rel = EventRelation.objects.create_relation(event, self)
+            rel.save()
+            try:
+                cal = Calendar.objects.get(slug='boarding_calender')
+            except Calendar.DoesNotExist:
+                cal = Calendar(
+                    name="Boarding Calender",
+                    slug="boarding_calender"
+                )
+                cal.save()
+            cal.events.add(event)
+        else:
+            event = Event.objects.get_for_object(self)[0]
+            event.start = self.start_date
+            event.end = self.end_date
+            event.title = title
+            event.description = description
+            event.save()
+
+    def get_absolute_url(self):
+        return reverse('app:boarding-detail', args=(self.id,))
