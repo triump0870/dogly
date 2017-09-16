7269a1fa4d3605aa1f800612e890238791af04b2
Rohan Roy <b3you0870@gmail.com>
2017-09-17 03:57:44 +0530

    BoardingCalendar utility method was added

---
+++ src/app/utils.py
@@ -0,0 +1,38 @@
+from calendar import HTMLCalendar
+from datetime import date
+from itertools import groupby
+
+
+class BoardingCalendar(HTMLCalendar):
+    def __init__(self, visits):
+        super(BoardingCalendar, self).__init__()
+        self.visits = self.group_by_day(visits)
+
+    def formatday(self, day, weekday):
+        if day != 0:
+            cssclass = self.cssclasses[weekday]
+            if date.today() == date(self.year, self.month, day):
+                cssclass += ' today'
+            count = 0
+            body = ['<p>']
+
+            for visit in self.visits.keys():
+                if day in range(visit[0], visit[1] + 1):
+                    count += 1
+            body.append('%s %s' % (count, 'Dog' if count == 1 else 'Dogs'))
+            return self.day_cell(cssclass, '%d %s' % (day, ''.join(body)))
+        return self.day_cell('noday', '&nbsp;')
+
+    def formatmonth(self, year, month):
+        self.year, self.month = year, month
+        return super(BoardingCalendar, self).formatmonth(year, month)
+
+    def group_by_day(self, visits):
+        field = lambda visit: (visit.start_date.day, visit.end_date.day)
+
+        return dict(
+            [(day, list(items)) for day, items in groupby(visits, field)]
+        )
+
+    def day_cell(self, cssclass, body):
+        return '<td class="%s">%s</td>' % (cssclass, body)
