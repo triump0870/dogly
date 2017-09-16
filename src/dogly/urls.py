c31885166892a7923cbd383ee84445d08eb02ddb
Rohan Roy <b3you0870@gmail.com>
2017-09-17 03:58:09 +0530

    project urls were modified

--- src/dogly/urls.py
+++ src/dogly/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="homepage.html"),),
     url(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
     url(r'^schedule/', include('schedule.urls'), name='scheduler'),
-
+    url(r'^boardings/', include('app.urls', namespace='app')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
