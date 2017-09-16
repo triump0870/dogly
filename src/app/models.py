6ccd2cb4b10c1e4eeaa1ab0b12e2f107fcd918a4
Rohan Roy <b3you0870@gmail.com>
2017-09-17 03:53:05 +0530

    Dog model was added

--- src/app/models.py
+++ src/app/models.py
@@ -1,5 +1,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.db.models import Q
+from django.db.utils import IntegrityError
+from schedule.models import Event, Calendar, EventRelation
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
+class Dog(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('first_name', 'last_name')
+
+    def __unicode__(self):
+        return "%s %s" % (str(self.first_name.title()), str(self.last_name.title()))
+
+    def save(self, *args, **kwargs):
+        self.first_name = self.first_name.lower()
+        if self.last_name:
+            self.last_name = self.last_name.lower()
+        try:
+            super(Dog, self).save(*args, **kwargs)
+        except IntegrityError as e:
+            print "error"
+            raise IntegrityError("Dog with name [%s %s] already exists" % (self.first_name, self.last_name))
+            
