603fe64b19d4c8d3950bc2c50d95779b30213f8e
Rohan Roy <b3you0870@gmail.com>
2017-09-17 15:28:38 +0530

    load_sample_data was added

--- src/app/management/commands/load_sample_data.py
+++ src/app/management/commands/load_sample_data.py
@@ -1,74 +1,101 @@
-try:
-    from django.core.management.base import NoArgsCommand as BaseCommand
-except ImportError:
-    from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand
+import random
+import string
+import calendar
+from app.models import Visit, Dog
+from datetime import timedelta, date, datetime
+import holidays
 
 
 class Command(BaseCommand):
-    help = "Load some sample data into the db"
+    """ Generate fake data"""
 
-    def handle(self, **options):
-        import datetime
-        from schedule.models import Calendar
-        from schedule.models import Event
-        from schedule.models import Rule
+    def favorable_days(self, date):
+        first_day_of_month = datetime(date.year, date.month, 1)
+        last_day_of_month = calendar.monthrange(date.year, date.month)[1]
+        # print "last day: ",
+        first_friday = first_day_of_month + timedelta(
+            days=((1 - calendar.monthrange(date.year, date.month)[0]) + 7) % 7)
+        # 4 is friday of week
+        days = []
+        days.append(first_friday)
+        days.append(first_friday + timedelta(days=1))
+        for day in [7, 14, 21, 28]:
+            friday = first_friday + timedelta(days=day)
+            if friday.day <= last_day_of_month:
+                days.append(friday)
+            saturday = first_friday + timedelta(days=day + 1)
+            if saturday.day <= last_day_of_month:
+                days.append(saturday)
+        return days
 
-        print("checking for existing data ...")
+    def holidays_list(self, year):
+        holidays_list = holidays.US(years=year)
+        holidays_dict = {}
+        for day in holidays_list:
+            holiday = {'start': day - timedelta(days=2), 'end': day + timedelta(days=2)}
+            holidays_dict[day.strftime("%Y-%m-%d")] = holiday
+        return holidays_dict
+
+    def create_visit(self, dog, start_date, end_date):
         try:
-            Calendar.objects.get(name="yml_cal")
-            print("It looks like you already have loaded the sample data, quitting.")
-            import sys
-            sys.exit(1)
-        except Calendar.DoesNotExist:
-            print("Sample data not found in db.")
-            print("Install it...")
+            visit = Visit(
+                dog=dog,
+                start_date=start_date,
+                end_date=end_date
+            )
+            visit.save()
+            print visit
+        except Exception as e:
+            # print "error: ", e.message
+            pass
 
-        print("Create 2 calendars : tony_cal, yml_cal")
-        yml_cal = Calendar(name="yml_cal", slug="yml")
-        yml_cal.save()
-        print("First calendar is created")
-        tony_cal = Calendar(name="tony_cal", slug="tony")
-        tony_cal.save()
-        print("Second calendar is created")
-        print("Do we need to create the most common rules?")
+    def handle(self, *args, **options):
         try:
-            rule = Rule.objects.get(name="Daily")
-        except Rule.DoesNotExist:
-            rule = Rule(frequency="YEARLY", name="Yearly", description="will recur once every Year")
-            rule.save()
-            print("YEARLY recurrence created")
-            rule = Rule(frequency="MONTHLY", name="Monthly", description="will recur once every Month")
-            rule.save()
-            print("Monthly recurrence created")
-            rule = Rule(frequency="WEEKLY", name="Weekly", description="will recur once every Week")
-            rule.save()
-            print("Weekly recurrence created")
-            rule = Rule(frequency="DAILY", name="Daily", description="will recur once every Day")
-            rule.save()
-            print("Daily recurrence created")
-        print("The common rules are installed.")
-        today = datetime.date.today()
+            Visit.objects.all().delete()
+            Dog.objects.all().delete()
+        except Exception as e:
+            print "Error: ", e.message
 
-        print("Create some events")
-        rule = Rule.objects.get(frequency="WEEKLY")
-        data = {
-            'title': 'Ping pong',
-            'start': datetime.datetime(today.year, today.month, today.day, 8, 0),
-            'end': datetime.datetime(today.year, today.month, today.day, 9, 0),
-            'end_recurring_period': datetime.datetime(today.year + 2, 5, 5, 0, 0),
-            'rule': rule,
-            'calendar': tony_cal
-        }
-        event = Event(**data)
-        event.save()
-        rule = Rule.objects.get(frequency="DAILY")
-        data = {
-            'title': 'Home work',
-            'start': datetime.datetime(today.year, today.month, today.day, 18, 0),
-            'end': datetime.datetime(today.year, today.month, today.day, 19, 0),
-            'end_recurring_period': datetime.datetime(today.year + 10, 5, 5, 0, 0),
-            'rule': rule,
-            'calendar': tony_cal
-        }
-        event = Event(**data)
-        event.save()
+        year = 2016
+        dogs = ["{} {}".format("".join([random.SystemRandom().choice(string.ascii_letters) for i in range(10)]),
+                               "".join([random.SystemRandom().choice(string.ascii_letters) for i in range(10)]))
+                for _ in range(10)]
+
+        for dog in dogs:
+            first_name, last_name = dog.split(' ')
+            try:
+                dog = Dog(
+                    first_name=first_name,
+                    last_name=last_name
+                )
+                dog.save()
+            except Exception as e:
+                # print "Error: ", e.message
+                pass
+        dogs = Dog.objects.all()
+        holidays = self.holidays_list(year)
+
+        for month in xrange(1, 13):
+            month_days = calendar.monthrange(year, month)[1]
+            favorable_days = self.favorable_days(date(year, month, 1))
+            print "favalorable days:", favorable_days
+            for day in range(1, month_days + 1):
+                start_date = date(year, month, day)
+                for _ in range(5):
+                    start_date = random.SystemRandom().choice(favorable_days)
+                    delta = timedelta(days=random.randrange(1, 20))
+                    end_date = start_date + delta
+                    dog = random.SystemRandom().choice(dogs)
+                    self.create_visit(dog, start_date, end_date)
+
+                if start_date.strftime("%Y-%m-%d") in holidays.keys():
+                    start_date, end_date = holidays[start_date.strftime("%Y-%m-%d")].values()
+                    for _ in range(3):
+                        dog = random.SystemRandom().choice(dogs)
+                        self.create_visit(dog, start_date, end_date)
+
+                delta = timedelta(days=random.randrange(1, 20))
+                end_date = start_date + delta
+                dog = random.SystemRandom().choice(dogs)
+                self.create_visit(dog, date(2016, 01, 01), end_date)
