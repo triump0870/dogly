017930a97d2034bd66bbd0f81b98082b8e96e55a
Rohan Roy <b3you0870@gmail.com>
2017-09-25 20:08:11 +0530

    urls naming was modified

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from app.views import boarding_list, in_the_house_view, load_sample_data_view
+from app.views import calendar, boardings, load_sample_data_view
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', boarding_list, name='boarding-list'),
-    url(r'^in-the-house/$', in_the_house_view, name='in-the-house'),
-    url(r'^load/load_sample_data/$', load_sample_data_view, name='load_sample_data_view'),
+    url(r'^$', calendar, name='calendar'),
+    url(r'^boardings/$', boardings, name='boardings'),
+    url(r'^load-sample-data/$', load_sample_data_view, name='load-sample-data'),
 ]
